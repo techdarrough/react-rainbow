96aa26584e549e1825e8c2b17e5d08ab7434bb11
Preston Darrough <techdarrough@gmail.com>
2022-04-05 15:26:37 -0700

    react-rainbows color bars with ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ColorBlock from './components/ColorBlock'
+import ColorForm from './components/ColorForm'
+
+export default function App(){
+    let [colors, setColors] = useState([
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ])
+
+    let colorMap = colors.map((color, i) => {
+      return (
+          <ColorBlock key={color } color={color} />
+      )
+  })
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
 }
 
-export default App;
+return (
+    <div className="App">
+        {colorMap}
+        <ColorForm addColor={addColor} />
+    </div>
+)
+  
+
+
+  
+}
+
+
+
+
+
+
+
+
